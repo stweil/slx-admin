044631ed2ac590a5e67ba4ac07267e790f814863
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2013-10-18 13:20:31 +0200

    Show different content on main page depending on logged in/guest status

--- inc/message.inc.php
+++ inc/message.inc.php
@@ -18,8 +18,33 @@ class Message
 		);
 	}
 
+	public static function addWarning($id)
+	{
+		self::$list[] = array(
+			'type' => 'warning',
+			'id'    => $id
+		);
+	}
+
+	public static function addInfo($id)
+	{
+		self::$list[] = array(
+			'type' => 'info',
+			'id'    => $id
+		);
+	}
+
+	public static function addSuccess($id)
+	{
+		self::$list[] = array(
+			'type' => 'success',
+			'id'    => $id
+		);
+	}
+
 	public static function renderList()
 	{
+		global $error_text;
 		foreach (self::$list as $item) {
 			Render::addTemplate('messagebox-' . $item['type'], array('message' => $error_text[$item['id']]));
 		}
--- modules/main.inc.php
+++ modules/main.inc.php
@@ -4,11 +4,16 @@ User::load();
 
 function render_module()
 {
-	Render::setTitle('Wurstgesicht');
+	// Render::setTitle('abc');
 	
 	Render::openTag('h1', array('class' => 'wurst kacke'));
 	Render::closeTag('h1');
 	
-	Render::addTemplate('helloworld', array('wurst' => 'käse & bier'));
+	if (!User::isLoggedIn()) {
+		Render::addTemplate('page-main-guest');
+		return;
+	}
+	// Logged in here
+	Render::addTemplate('page-main', array('user' => User::getName()));
 }
 
