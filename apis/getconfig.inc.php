eec83c9545d8a9a06ac0f63846eca68e2280a83d
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2013-12-17 15:25:12 +0100

    Deliver remote log url to clients via config

--- apis/getconfig.inc.php
+++ apis/getconfig.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+// Dump config from DB
 $res = Database::simpleQuery('SELECT setting.setting, setting.defaultvalue, setting.permissions, setting.description, tbl.value
 	FROM setting
 	LEFT JOIN setting_global AS tbl USING (setting)
@@ -8,3 +9,6 @@ while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
 	if (empty($row['value'])) $row['value'] = $row['defaultvalue'];
 	echo $row['setting'] . "='" . str_replace("'", "'\"'\"'", $row['value']) . "'\n";
 }
+// Additional "intelligent" config
+echo "SLX_REMOTE_LOG='http://${_SERVER['SERVER_ADDR']}/slxadmin/api.php?do=clientlog'\n";
+
