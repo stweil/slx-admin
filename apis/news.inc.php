49af56d7262c58e79a67ad2f6d3ef02b9a05cbd6
Jonathan Bauer <bauer@informatik.uni-freiburg.de>
2014-06-05 17:37:37 +0200

    news api for vmchooser2

---
+++ apis/news.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Content-Type: application/xml; charset=utf-8');
+
+// Fetch news from DB
+$row = Database::queryFirst('SELECT title, content, dateline FROM news ORDER BY dateline DESC LIMIT 1');
+if ($row !== false ) {
+
+	echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+	echo "<news>" . "\n";
+	echo "\t" . '<headline>' . "\n";
+	echo "\t\t" . $row['title'] . "\n";
+	echo "\t" . '</headline>' . "\n";
+	echo "\t" . "<info>" . "\n";
+	echo "\t\t" . $row['content'] . "\n";
+	echo "\t" . '</info>' . "\n";
+	echo "\t" . "<date>" . "\n";
+	echo "\t\t" . $row['dateline'] . "\n";
+	echo "\t" . "</date>" . "\n";
+	echo "</news>";
+
+}
+
