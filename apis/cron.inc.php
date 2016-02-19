1e57f42bde0d2cc11f77ff834c9d6d3631ac27cb
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-02-19 15:02:08 +0100

    [statistics] cron: log usage

--- apis/cron.inc.php
+++ apis/cron.inc.php
@@ -31,5 +31,20 @@ case 6:
 	break;
 }
 
+// TODO: Move to some module
+function logstats() {
+	$NOW = time();
+	$cutoff = $NOW - 86400 * 30;
+	$online = $NOW - 610;
+	$known = Database::queryFirst("SELECT Count(*) AS val FROM machine WHERE lastseen > $cutoff");
+	$on = Database::queryFirst("SELECT Count(*) AS val FROM machine WHERE lastseen > $online");
+	$used = Database::queryFirst("SELECT Count(*) AS val FROM machine WHERE lastseen > $online AND logintime <> 0");
+	Database::exec("INSERT INTO statistic (dateline, typeid, clientip, username, data) VALUES (:now, '~stats', '', '', :vals)", array(
+		'now' => $NOW,
+		'vals' => $known['val'] . '#' . $on['val'] . '#' . $used['val'],
+	));
+}
+logstats();
+
 Trigger::checkCallbacks();
 Trigger::ldadp();
