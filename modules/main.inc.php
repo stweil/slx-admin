19b1fc7a8a2be4b7ba600ce73e50fc87562de274
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-02-22 09:58:53 +0100

    sd-gkmdth DTHOSTROJSTDH

--- modules/main.inc.php
+++ modules/main.inc.php
@@ -16,7 +16,7 @@ class Page_Main extends Page
 			$this->sysconfig = !file_exists(CONFIG_HTTP_DIR . '/default/config.tgz');
 			$this->minilinux = !file_exists(CONFIG_HTTP_DIR . '/default/kernel') || !file_exists(CONFIG_HTTP_DIR . '/default/initramfs-stage31') || !file_exists(CONFIG_HTTP_DIR . '/default/stage32.sqfs');
 			$this->vmstore = !is_array(Property::getVmStoreConfig());
-			$this->ipxe = true || !preg_match('/^\d+\.\d+\.\d+\.\d+$/', Property::getServerIp());
+			$this->ipxe = !preg_match('/^\d+\.\d+\.\d+\.\d+$/', Property::getServerIp());
 			Property::setNeedsSetup(($this->sysconfig || $this->minilinux || $this->vmstore || $this->ipxe) ? 1 : 0);
 			$res = Database::queryFirst("SELECT Count(*) AS cnt FROM sat.imageversion WHERE deletestate = 'SHOULD_DELETE'");
 			$this->delPending = isset($res['cnt']) ? $res['cnt'] : 0;
