ce053dbabd38bae88a513dee408dd41351fd986a
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-07-05 13:31:37 +0200

    [minilinux] Fix link not showing in main-warning hook

--- modules-available/minilinux/hooks/main-warning.inc.php
+++ modules-available/minilinux/hooks/main-warning.inc.php
@@ -1,6 +1,6 @@
 <?php
 
 if (!file_exists(CONFIG_HTTP_DIR . '/default/kernel') || !file_exists(CONFIG_HTTP_DIR . '/default/initramfs-stage31') || !file_exists(CONFIG_HTTP_DIR . '/default/stage32.sqfs')) {
-	Message::addError('minilinux.please-download-minilinux');
+	Message::addError('minilinux.please-download-minilinux', true);
 	$needSetup = true;
 }
