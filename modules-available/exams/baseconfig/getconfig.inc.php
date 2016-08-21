20431c95164cdceda3e0cf5aa22090a8c9c2efe1
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-08-21 17:52:42 +0200

    [exams] Fix exam mode detection for global location/clients without location

--- modules-available/exams/baseconfig/getconfig.inc.php
+++ modules-available/exams/baseconfig/getconfig.inc.php
@@ -2,10 +2,12 @@
 
 if (isset($configVars["SLX_LOCATIONS"])) {
 	$locationIds = explode(' ', $configVars["SLX_LOCATIONS"]);
-	if (Exams::isInExamMode($locationIds, $lectureId)) {
-		$configVars['SLX_EXAM'] = 'yes';
-		if (strlen($lectureId) > 0) {
-			$configVars['SLX_EXAM_START'] = $lectureId;
-		}
+} else {
+	$locationIds = array();
+}
+if (Exams::isInExamMode($locationIds, $lectureId)) {
+	$configVars['SLX_EXAM'] = 'yes';
+	if (strlen($lectureId) > 0) {
+		$configVars['SLX_EXAM_START'] = $lectureId;
 	}
 }
