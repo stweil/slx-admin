cedbd258ead3a2783f5c7c3e923ef6a4e5a1459f
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-11-14 15:51:45 +0100

    [roomplanner] baseconfig hook: Add variable for hybrid mode

--- modules-available/roomplanner/baseconfig/getconfig.inc.php
+++ modules-available/roomplanner/baseconfig/getconfig.inc.php
@@ -3,6 +3,10 @@
 $configVars["SLX_PVS_CONFIG_URL"] = 'http://' . $_SERVER['SERVER_ADDR'] . $_SERVER['SCRIPT_NAME'] . '?do=roomplanner';
 
 $res = Database::queryFirst('SELECT dedicatedmgr FROM location_roomplan WHERE managerip = :ip LIMIT 1', ['ip' => $ip]);
-if ($res !== false && (int)$res['dedicatedmgr'] !== 0) {
-	$configVars["SLX_PVS_DEDICATED"] = 'yes';
+if ($res !== false) {
+	if ((int)$res['dedicatedmgr'] !== 0) {
+		$configVars["SLX_PVS_DEDICATED"] = 'yes';
+	} else {
+		$configVars["SLX_PVS_HYBRID"] = 'yes';
+	}
 }
