0fca0765b73724616c545345cbaa056bcd369292
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-01-18 15:20:02 +0100

    [locationinfo] Fix formatting

--- modules-available/locationinfo/inc/locationinfo.inc.php
+++ modules-available/locationinfo/inc/locationinfo.inc.php
@@ -3,27 +3,27 @@
 class LocationInfo
 {
 
-  public static function getPcState($logintime, $lastseen) {
-    /*   pcState:
-     *  [0] =  IDLE (NOT IN USE)
-     *  [1] = OCCUPIED (IN USE)
-     *  [2] = OFF
-     *  [3] = 10 days offline (BROKEN?)
-     */
+	public static function getPcState($logintime, $lastseen)
+	{
+		/*   pcState:
+		 *  [0] =  IDLE (NOT IN USE)
+		 *  [1] = OCCUPIED (IN USE)
+		 *  [2] = OFF
+		 *  [3] = 10 days offline (BROKEN?)
+		 */
 
-     $NOW = time();
+		$NOW = time();
 
-
-    if ($NOW - $lastseen > 864000) {
-      return 3;
-    } elseif ($NOW - $lastseen > 610) {
-      return 2;
-    } elseif ($logintime == 0) {
-      return 0;
-    } elseif ($logintime > 0) {
-      return 1;
-    }
-    return -1;
-  }
+		if ($NOW - $lastseen > 864000) {
+			return 3;
+		} elseif ($NOW - $lastseen > 610) {
+			return 2;
+		} elseif ($logintime == 0) {
+			return 0;
+		} elseif ($logintime > 0) {
+			return 1;
+		}
+		return -1;
+	}
 
 }
