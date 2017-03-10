950ea6ead0dc3a1ba59db580f2a2b1977165375d
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-03-10 12:40:00 +0100

    [sysconfig] configtgz.inc.php: Move static functions to bottom

--- modules-available/sysconfig/inc/configtgz.inc.php
+++ modules-available/sysconfig/inc/configtgz.inc.php
@@ -82,138 +82,6 @@ class ConfigTgz
 		));
 		return true;
 	}
-		
-	public static function insert($title, $moduleIds)
-	{
-		if (!is_array($moduleIds))
-			return false;
-		$instance = new ConfigTgz;
-		$instance->configTitle = $title;
-		// Create output file name (config.tgz)
-		do {
-			$instance->file = CONFIG_TGZ_LIST_DIR . '/config-' . Util::sanitizeFilename($instance->configTitle) . '-' . mt_rand() . '-' . time() . '.tgz';
-		} while (file_exists($instance->file));
-		Database::exec("INSERT INTO configtgz (title, filepath, status) VALUES (:title, :filepath, :status)", array(
-			'title' => $instance->configTitle,
-			'filepath' => $instance->file,
-			'status' => 'MISSING'
-		));
-		$instance->configId = Database::lastInsertId();
-		$instance->modules = array();
-		// Get all modules to put in config
-		$idstr = '0'; // Passed directly in query. Make sure no SQL injection is possible
-		foreach ($moduleIds as $module) {
-			$idstr .= ',' . (int)$module; // Casting to int should make it safe
-		}
-		$res = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_module WHERE moduleid IN ($idstr)");
-		// Make connection
-		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-			Database::exec("INSERT INTO configtgz_x_module (configid, moduleid) VALUES (:configid, :moduleid)", array(
-				'configid' => $instance->configId,
-				'moduleid' => $row['moduleid']
-			));
-			$instance->modules[] = $row;
-		}
-		return $instance;
-	}
-	
-	public static function get($configId)
-	{
-		$ret = Database::queryFirst("SELECT configid, title, filepath FROM configtgz WHERE configid = :configid", array(
-			'configid' => $configId
-		));
-		if ($ret === false)
-			return false;
-		$instance = new ConfigTgz;
-		$instance->configId = $ret['configid'];
-		$instance->configTitle = $ret['title'];
-		$instance->file = $ret['filepath'];
-		$ret = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_x_module "
-			. " INNER JOIN configtgz_module USING (moduleid) "
-			. " WHERE configid = :configid", array('configid' => $instance->configId));
-		$instance->modules = array();
-		while ($row = $ret->fetch(PDO::FETCH_ASSOC)) {
-			$instance->modules[] = $row;
-		}
-		return $instance;
-	}
-	
-	public static function getAllForModule($moduleId)
-	{
-		$res = Database::simpleQuery("SELECT configid, title, filepath FROM configtgz_x_module "
-			. " INNER JOIN configtgz USING (configid) "
-			. " WHERE moduleid = :moduleid", array(
-			'moduleid' => $moduleId
-		));
-		if ($res === false)
-			return false;
-		$list = array();
-		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-			$instance = new ConfigTgz;
-			$instance->configId = $row['configid'];
-			$instance->configTitle = $row['title'];
-			$instance->file = $row['filepath'];
-			$innerRes = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_x_module "
-				. " INNER JOIN configtgz_module USING (moduleid) "
-				. " WHERE configid = :configid", array('configid' => $instance->configId));
-			$instance->modules = array();
-			while ($innerRow = $innerRes->fetch(PDO::FETCH_ASSOC)) {
-				$instance->modules[] = $innerRow;
-			}
-			$list[] = $instance;
-		}
-		return $list;
-	}
-
-	/**
-	 * Called when (re)generating a config tgz failed, so we can
-	 * update the status in the DB and add a server log entry.
-	 *
-	 * @param array $task
-	 * @param array $args contains 'configid' and optionally 'deleteOnError'
-	 */
-	public static function generateFailed($task, $args)
-	{
-		if (!isset($args['configid']) || !is_numeric($args['configid'])) {
-			EventLog::warning('Ignoring generateFailed event as it has no configid assigned.');
-			return;
-		}
-		$config = self::get($args['configid']);
-		if ($config === false) {
-			EventLog::warning('generateFailed callback for config id ' . $args['configid'] . ', but no instance could be generated.');
-			return;
-		}
-		if (isset($task['data']['error']))
-			$error = $task['data']['error'];
-		elseif (isset($task['data']['messages']))
-			$error = $task['data']['messages'];
-		else
-			$error = '';
-		EventLog::failure("Generating config.tgz '" . $config->configTitle . "' failed.", $error);
-		if ($args['deleteOnError'])
-			$config->delete();
-		else
-			$config->markFailed();
-	}
-
-	/**
-	 * (Re)generating a config tgz succeeded. Update db entry.
-	 *
-	 * @param array $args contains 'configid' and optionally 'deleteOnError'
-	 */
-	public static function generateSucceeded($args)
-	{
-		if (!isset($args['configid']) || !is_numeric($args['configid'])) {
-			EventLog::warning('Ignoring generateSucceeded event as it has no configid assigned.');
-			return;
-		}
-		$config = self::get($args['configid']);
-		if ($config === false) {
-			EventLog::warning('generateSucceeded callback for config id ' . $args['configid'] . ', but no instance could be generated.');
-			return;
-		}
-		$config->markUpdated();
-	}
 	
 	/**
 	 * 
@@ -324,6 +192,142 @@ class ConfigTgz
 			'status' => $status
 		));
 		return $status;
+	}
+
+	/*
+	 * Static part
+	 */
+
+	public static function insert($title, $moduleIds)
+	{
+		if (!is_array($moduleIds))
+			return false;
+		$instance = new ConfigTgz;
+		$instance->configTitle = $title;
+		// Create output file name (config.tgz)
+		do {
+			$instance->file = CONFIG_TGZ_LIST_DIR . '/config-' . Util::sanitizeFilename($instance->configTitle) . '-' . mt_rand() . '-' . time() . '.tgz';
+		} while (file_exists($instance->file));
+		Database::exec("INSERT INTO configtgz (title, filepath, status) VALUES (:title, :filepath, :status)", array(
+			'title' => $instance->configTitle,
+			'filepath' => $instance->file,
+			'status' => 'MISSING'
+		));
+		$instance->configId = Database::lastInsertId();
+		$instance->modules = array();
+		// Get all modules to put in config
+		$idstr = '0'; // Passed directly in query. Make sure no SQL injection is possible
+		foreach ($moduleIds as $module) {
+			$idstr .= ',' . (int)$module; // Casting to int should make it safe
+		}
+		$res = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_module WHERE moduleid IN ($idstr)");
+		// Make connection
+		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+			Database::exec("INSERT INTO configtgz_x_module (configid, moduleid) VALUES (:configid, :moduleid)", array(
+				'configid' => $instance->configId,
+				'moduleid' => $row['moduleid']
+			));
+			$instance->modules[] = $row;
+		}
+		return $instance;
+	}
+
+	public static function get($configId)
+	{
+		$ret = Database::queryFirst("SELECT configid, title, filepath FROM configtgz WHERE configid = :configid", array(
+			'configid' => $configId
+		));
+		if ($ret === false)
+			return false;
+		$instance = new ConfigTgz;
+		$instance->configId = $ret['configid'];
+		$instance->configTitle = $ret['title'];
+		$instance->file = $ret['filepath'];
+		$ret = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_x_module "
+			. " INNER JOIN configtgz_module USING (moduleid) "
+			. " WHERE configid = :configid", array('configid' => $instance->configId));
+		$instance->modules = array();
+		while ($row = $ret->fetch(PDO::FETCH_ASSOC)) {
+			$instance->modules[] = $row;
+		}
+		return $instance;
+	}
+
+	public static function getAllForModule($moduleId)
+	{
+		$res = Database::simpleQuery("SELECT configid, title, filepath FROM configtgz_x_module "
+			. " INNER JOIN configtgz USING (configid) "
+			. " WHERE moduleid = :moduleid", array(
+			'moduleid' => $moduleId
+		));
+		if ($res === false)
+			return false;
+		$list = array();
+		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+			$instance = new ConfigTgz;
+			$instance->configId = $row['configid'];
+			$instance->configTitle = $row['title'];
+			$instance->file = $row['filepath'];
+			$innerRes = Database::simpleQuery("SELECT moduleid, filepath, status FROM configtgz_x_module "
+				. " INNER JOIN configtgz_module USING (moduleid) "
+				. " WHERE configid = :configid", array('configid' => $instance->configId));
+			$instance->modules = array();
+			while ($innerRow = $innerRes->fetch(PDO::FETCH_ASSOC)) {
+				$instance->modules[] = $innerRow;
+			}
+			$list[] = $instance;
+		}
+		return $list;
+	}
+
+	/**
+	 * Called when (re)generating a config tgz failed, so we can
+	 * update the status in the DB and add a server log entry.
+	 *
+	 * @param array $task
+	 * @param array $args contains 'configid' and optionally 'deleteOnError'
+	 */
+	public static function generateFailed($task, $args)
+	{
+		if (!isset($args['configid']) || !is_numeric($args['configid'])) {
+			EventLog::warning('Ignoring generateFailed event as it has no configid assigned.');
+			return;
+		}
+		$config = self::get($args['configid']);
+		if ($config === false) {
+			EventLog::warning('generateFailed callback for config id ' . $args['configid'] . ', but no instance could be generated.');
+			return;
+		}
+		if (isset($task['data']['error']))
+			$error = $task['data']['error'];
+		elseif (isset($task['data']['messages']))
+			$error = $task['data']['messages'];
+		else
+			$error = '';
+		EventLog::failure("Generating config.tgz '" . $config->configTitle . "' failed.", $error);
+		if ($args['deleteOnError'])
+			$config->delete();
+		else
+			$config->markFailed();
+	}
+
+	/**
+	 * (Re)generating a config tgz succeeded. Update db entry.
+	 *
+	 * @param array $args contains 'configid' and optionally 'deleteOnError'
+	 */
+	public static function generateSucceeded($args)
+	{
+		if (!isset($args['configid']) || !is_numeric($args['configid'])) {
+			EventLog::warning('Ignoring generateSucceeded event as it has no configid assigned.');
+			return;
+		}
+		$config = self::get($args['configid']);
+		if ($config === false) {
+			EventLog::warning('generateSucceeded callback for config id ' . $args['configid'] . ', but no instance could be generated.');
+			return;
+		}
+		$config->markUpdated();
 	}
 	
 }
