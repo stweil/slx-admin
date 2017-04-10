76d9ed010bd72fa01c39c7e66a90546d70be0c10
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-04-10 12:03:16 +0200

    [apis/cron] Try to catch exceptions when running module cron handler

--- apis/cron.inc.php
+++ apis/cron.inc.php
@@ -52,6 +52,11 @@ foreach (Hook::load('cron') as $hook) {
 	}
 	$value = $hook->moduleId . '|' . time();
 	Property::addToList(CRON_KEY_STATUS, $value, 1800);
-	handleModule($hook->file);
+	try {
+		handleModule($hook->file);
+	} catch (Exception $e) {
+		// Logging
+		EventLog::failure('Cronjob for module ' . $hook->moduleId . ' has crashed. Check the php or web server error log.', $e->toString());
+	}
 	Property::removeFromList(CRON_KEY_STATUS, $value);
 }
