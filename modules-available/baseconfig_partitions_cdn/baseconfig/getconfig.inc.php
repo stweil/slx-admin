06183776d0397bc3126a85d0b9380600dadf7972
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-07-12 18:37:39 +0200

    [baseconfig_partitions_cdn] Fix warning

--- modules-available/baseconfig_partitions_cdn/baseconfig/getconfig.inc.php
+++ modules-available/baseconfig_partitions_cdn/baseconfig/getconfig.inc.php
@@ -1,17 +1,19 @@
 <?php
 
-$config = '';
-$res = Database::simpleQuery('SELECT partition_id, size, mount_point, options FROM setting_partition WHERE user = :user',
-	array('user'=>$_GET['user']));
-while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-	$config .= "\n" . $row['partition_id'] . "," . $row['size'];
-	if(strlen($row['mount_point']) > 0)
-		$config .= "," . $row['mount_point'];
-	if(strlen($row['options']) > 0)
-		$config .= "," . $row['options'];
-}
-$config .= "\n";
+if (isset($_GET['user'])) {
+	$config = '';
+	$res = Database::simpleQuery('SELECT partition_id, size, mount_point, options FROM setting_partition WHERE user = :user',
+		array('user' => $_GET['user']));
+	while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+		$config .= "\n" . $row['partition_id'] . "," . $row['size'];
+		if (strlen($row['mount_point']) > 0)
+			$config .= "," . $row['mount_point'];
+		if (strlen($row['options']) > 0)
+			$config .= "," . $row['options'];
+	}
+	$config .= "\n";
 
 // vm list url. doesn't really fit anywhere, seems to be a tie between here and dozmod
-$configVars["SLX_PARTITION_TABLE"] = $config;
-unset($config);
+	$configVars["SLX_PARTITION_TABLE"] = $config;
+	unset($config);
+}
