1f95b689d8c4b46cc055b45ad12dfa0c0c8794cf
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-11-14 15:04:48 +0100

    [exams/roomplanner] Smarter coordination of run mode (exam mode on/off etc)

--- modules-available/roomplanner/baseconfig/getconfig.inc.php
+++ modules-available/roomplanner/baseconfig/getconfig.inc.php
@@ -5,7 +5,10 @@ ConfigHolder::add("SLX_PVS_CONFIG_URL", 'http://' . $_SERVER['SERVER_ADDR'] . $_
 $res = Database::queryFirst('SELECT dedicatedmgr FROM location_roomplan WHERE managerip = :ip LIMIT 1', ['ip' => $ip]);
 if ($res !== false) {
 	if ((int)$res['dedicatedmgr'] !== 0) {
+		// TODO: Runmode
 		ConfigHolder::add("SLX_PVS_DEDICATED", 'yes');
+		ConfigHolder::add("SLX_EXAM", false, 100000);
+		ConfigHolder::add("SLX_SYSTEMD_TARGET", false, 100000);
 	} else {
 		ConfigHolder::add("SLX_PVS_HYBRID", 'yes');
 	}
