9f7e2c4c794495f2a32410fd373cc313e0b0d4cf
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2017-11-15 15:16:51 +0100

    [statistics&syslog] Fix possible cron delete query overflow

--- modules-available/syslog/hooks/cron.inc.php
+++ modules-available/syslog/hooks/cron.inc.php
@@ -1,5 +1,8 @@
 <?php
 
 if (mt_rand(1, 10) === 1) {
-	Database::exec("DELETE FROM clientlog WHERE (UNIX_TIMESTAMP() - dateline) > 86400 * 190");
+	Database::exec("DELETE FROM clientlog WHERE (UNIX_TIMESTAMP() - 86400 * 190) > dateline");
+	if (mt_rand(1, 100) === 1) {
+		Database::exec("OPTIMIZE TABLE clientlog");
+	}
 }
