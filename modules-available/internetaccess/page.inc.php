dfca01bd4444478a9fa5a4f9778ee56b932f0e5c
Christian Hofmaier <you@example.com>
2017-11-21 17:28:34 +0100

    [internetaccess] reworked permission system from "click and you get error" to "button is disabled due to lack of permission"

--- modules-available/internetaccess/page.inc.php
+++ modules-available/internetaccess/page.inc.php
@@ -15,29 +15,24 @@ class Page_InternetAccess extends Page
 		$action = Request::any('action', 'show');
 
 		if ($action == 'save') {
-			if (User::hasPermission("configuration.save")) {
-				if (isset($_POST['PROXY_CONF'])) {
-					$data = array();
-					foreach (array('PROXY_CONF', 'PROXY_ADDR', 'PROXY_PORT', 'PROXY_USERNAME', 'PROXY_PASSWORD') as $key) {
-						$data[$key] = Request::post($key, '');
-					}
-					if (!FileUtil::arrayToFile(CONFIG_PROXY_CONF, $data)) {
-						Message::addError('main.error-write', CONFIG_PROXY_CONF);
-						Util::redirect();
-					} else {
-						Message::addSuccess('settings-updated');
-						Taskmanager::release(Taskmanager::submit('ReloadProxy'));
-						$taskids = array();
-						Trigger::stopDaemons(NULL, $taskids);
-						$taskids = array();
-						Trigger::startDaemons(NULL, $taskids);
-						Session::set('ia-restart', $taskids);
-						Util::redirect('?do=InternetAccess&show=update');
-					}
+			if (isset($_POST['PROXY_CONF'])) {
+				$data = array();
+				foreach (array('PROXY_CONF', 'PROXY_ADDR', 'PROXY_PORT', 'PROXY_USERNAME', 'PROXY_PASSWORD') as $key) {
+					$data[$key] = Request::post($key, '');
 				}
-			} else {
-				Message::addError('main.no-permission');
-				Util::redirect('?do=internetaccess');
+				if (!FileUtil::arrayToFile(CONFIG_PROXY_CONF, $data)) {
+					Message::addError('main.error-write', CONFIG_PROXY_CONF);
+					Util::redirect();
+				} else {
+					Message::addSuccess('settings-updated');
+					Taskmanager::release(Taskmanager::submit('ReloadProxy'));
+					$taskids = array();
+					Trigger::stopDaemons(NULL, $taskids);
+					$taskids = array();
+					Trigger::startDaemons(NULL, $taskids);
+					Session::set('ia-restart', $taskids);
+					Util::redirect('?do=InternetAccess&show=update');
+				}
 			}
 		}
 	}
@@ -56,6 +51,9 @@ class Page_InternetAccess extends Page
 		if (!isset($data['PROXY_CONF']))
 			$data['PROXY_CONF'] = 'AUTO';
 		$data['selected_' . $data['PROXY_CONF']] = 'selected';
+
+		$data['allowedSave'] = User::hasPermission("configuration.safe");
+
 		Render::addTemplate('_page', $data);
 	}
 
