c274a1545da0f09c1360734221e551d0ccd3c3b6
Udo Walter <udo.walter@mars.uni-freiburg.de>
2017-12-28 17:22:22 +0100

    [eventlog] added permission to view the log

--- modules-available/eventlog/page.inc.php
+++ modules-available/eventlog/page.inc.php
@@ -6,36 +6,43 @@ class Page_EventLog extends Page
 	protected function doPreprocess()
 	{
 		User::load();
-		if (!User::hasPermission('superadmin')) {
+		if (!User::isLoggedIn()) {
 			Message::addError('main.no-permission');
 			Util::redirect('?do=Main');
 		}
-		User::setLastSeenEvent(Property::getLastWarningId());
+		if (User::hasPermission("view")) {
+			User::setLastSeenEvent(Property::getLastWarningId());
+		}
 	}
 
 	protected function doRender()
 	{
-		$today = date('d.m.Y');
-		$yesterday = date('d.m.Y', time() - 86400);
-		$lines = array();
-		$paginate = new Paginate("SELECT logid, dateline, logtypeid, description, extra FROM eventlog ORDER BY logid DESC", 50);
-		$res = $paginate->exec();
-		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-			$day = date('d.m.Y', $row['dateline']);
-			if ($day === $today) {
-				$day = Dictionary::translate('lang_today');
-			} elseif ($day === $yesterday) {
-				$day = Dictionary::translate('lang_yesterday');
+		Render::addTemplate("heading");
+		if (User::hasPermission("view")) {
+			$today = date('d.m.Y');
+			$yesterday = date('d.m.Y', time() - 86400);
+			$lines = array();
+			$paginate = new Paginate("SELECT logid, dateline, logtypeid, description, extra FROM eventlog ORDER BY logid DESC", 50);
+			$res = $paginate->exec();
+			while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+				$day = date('d.m.Y', $row['dateline']);
+				if ($day === $today) {
+					$day = Dictionary::translate('lang_today');
+				} elseif ($day === $yesterday) {
+					$day = Dictionary::translate('lang_yesterday');
+				}
+				$row['date'] = $day . date(' H:i', $row['dateline']);
+				$row['icon'] = $this->typeToIcon($row['logtypeid']);
+				$row['color'] = $this->typeToColor($row['logtypeid']);
+				$lines[] = $row;
 			}
-			$row['date'] = $day . date(' H:i', $row['dateline']);
-			$row['icon'] = $this->typeToIcon($row['logtypeid']);
-			$row['color'] = $this->typeToColor($row['logtypeid']);
-			$lines[] = $row;
-		}
 
-		$paginate->render('_page', array(
-			'list' => $lines
-		));
+			$paginate->render('_page', array(
+				'list' => $lines
+			));
+		} else {
+			Message::addError('main.no-permission');
+		}
 	}
 
 	private function typeToIcon($type)
