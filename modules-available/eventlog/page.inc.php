556e7ee54269bd94bfb166bae59ef51ad93df8aa
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2018-02-12 18:49:36 +0100

    [eventlog] Use Util::prettyTime for timestamp formatting

--- modules-available/eventlog/page.inc.php
+++ modules-available/eventlog/page.inc.php
@@ -13,19 +13,11 @@ class Page_EventLog extends Page
 	protected function doRender()
 	{
 		Render::addTemplate("heading");
-		$today = date('d.m.Y');
-		$yesterday = date('d.m.Y', time() - 86400);
 		$lines = array();
 		$paginate = new Paginate("SELECT logid, dateline, logtypeid, description, extra FROM eventlog ORDER BY logid DESC", 50);
 		$res = $paginate->exec();
 		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-			$day = date('d.m.Y', $row['dateline']);
-			if ($day === $today) {
-				$day = Dictionary::translate('lang_today');
-			} elseif ($day === $yesterday) {
-				$day = Dictionary::translate('lang_yesterday');
-			}
-			$row['date'] = $day . date(' H:i', $row['dateline']);
+			$row['date'] = Util::prettyTime($row['dateline']);
 			$row['icon'] = $this->typeToIcon($row['logtypeid']);
 			$row['color'] = $this->typeToColor($row['logtypeid']);
 			$lines[] = $row;
