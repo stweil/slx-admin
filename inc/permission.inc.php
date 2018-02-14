c3a4746d44a3909e1281bbb575987888ae80ac7b
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2018-02-14 13:19:58 +0100

    [inc/Permission] Add parameter to helper; fix for querying foreign module

--- inc/permission.inc.php
+++ inc/permission.inc.php
@@ -16,22 +16,31 @@ class Permission
 	}
 
 
-	public static function addGlobalTags(&$array, $locationid, $disabled)
+	// TODO: Doc/Refactor
+	public static function addGlobalTags(&$array, $locationid, $disabled, $noneAvailDisabled = null)
 	{
 		if (!Module::isAvailable('permissionmanager'))
 			return;
+		$one = false;
 		foreach ($disabled as $perm) {
-			if (User::hasPermission($perm, $locationid))
+			if (User::hasPermission($perm, $locationid)) {
+				$one = true;
 				continue;
+			}
 			if (strpos($perm, '.') === false) {
 				$array[$perm]['disabled'] = 'disabled';
 				continue;
 			}
 			$temp =& $array;
 			foreach (explode('.', $perm) as $sub) {
+				if (empty($sub))
+					continue;
 				$temp =& $temp[$sub];
 			}
 			$temp['disabled'] = 'disabled';
+		}
+		if (!$one && !is_null($noneAvailDisabled)) {
+			$array[$noneAvailDisabled]['disabled'] = true;
 		}
 	}
 
