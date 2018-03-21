e551417b5d672f23c5f932e9599fa7495b4ca4ec
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2018-03-21 15:37:40 +0100

    [vmstore] Deliver mount options to clients
    refs #3323

--- modules-available/vmstore/baseconfig/getconfig.inc.php
+++ modules-available/vmstore/baseconfig/getconfig.inc.php
@@ -9,11 +9,17 @@ if (is_array($vmstore) && isset($vmstore['storetype'])) {
 		break;
 	case 'nfs';
 		ConfigHolder::add("SLX_VM_NFS", $vmstore['nfsaddr']);
+		if (!empty($vmstore['nfsopts'])) {
+			ConfigHolder::add("SLX_VM_NFS_OPTS", $vmstore['nfsopts']);
+		}
 		break;
 	case 'cifs';
 		ConfigHolder::add("SLX_VM_NFS", $vmstore['cifsaddr']);
 		ConfigHolder::add("SLX_VM_NFS_USER", $vmstore['cifsuserro']);
 		ConfigHolder::add("SLX_VM_NFS_PASSWD", $vmstore['cifspasswdro']);
+		if (!empty($vmstore['cifsopts'])) {
+			ConfigHolder::add("SLX_VM_NFS_OPTS", $vmstore['cifsopts']);
+		}
 		break;
 	}
 }
