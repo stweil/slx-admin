9a70136f17624d4ba52f5eaf233a8d50836ad4f9
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2018-03-28 14:29:47 +0200

    [dozmod] Hide expired image hint on main page if no permission
    Refs #3332

--- modules-available/dozmod/hooks/main-warning.inc.php
+++ modules-available/dozmod/hooks/main-warning.inc.php
@@ -4,8 +4,10 @@
  * Show notification in main window if there are images that should be deleted and are waiting for confirmation
  */
 
-$res = Database::queryFirst("SELECT Count(*) AS cnt FROM sat.imageversion WHERE deletestate = 'SHOULD_DELETE'", array(), true);
-if (isset($res['cnt']) && $res['cnt'] > 0) {
-	Message::addInfo('dozmod.images-pending-delete-exist', true, $res['cnt']);
+if (User::hasPermission('.dozmod.expiredimages.delete')) {
+	$res = Database::queryFirst("SELECT Count(*) AS cnt FROM sat.imageversion WHERE deletestate = 'SHOULD_DELETE'", array(), true);
+	if (isset($res['cnt']) && $res['cnt'] > 0) {
+		Message::addInfo('dozmod.images-pending-delete-exist', true, $res['cnt']);
+	}
+	unset($res);
 }
-unset($res);
