7a47438b8be68835f0033ea714e1eca7e6fb2a05
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2018-04-13 14:57:08 +0200

    [js_stupidtable] Add ipv4 sort function

--- modules-available/js_stupidtable/clientscript.js
+++ modules-available/js_stupidtable/clientscript.js
@@ -82,6 +82,16 @@
 			a = a.toString().toLocaleLowerCase();
 			b = b.toString().toLocaleLowerCase();
 			return a.localeCompare(b);
+		},
+		// OpenSLX -- IPv4 sort function
+		"ipv4":function(a,b){
+			var aa = a.split(".");
+			var bb = b.split(".");
+
+			var resulta = aa[0]*0x1000000 + aa[1]*0x10000 + aa[2]*0x100 + aa[3]*1;
+			var resultb = bb[0]*0x1000000 + bb[1]*0x10000 + bb[2]*0x100 + bb[3]*1;
+
+			return resulta-resultb;
 		}
 	};
 
