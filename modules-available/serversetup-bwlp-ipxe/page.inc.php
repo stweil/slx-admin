153062577e4de44c612b5e1a8003fc7c38420790
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2019-03-25 12:16:57 +0100

    [serversetup-bwlp-ipxe] On empty bootentry list, import subnets too

--- modules-available/serversetup-bwlp-ipxe/page.inc.php
+++ modules-available/serversetup-bwlp-ipxe/page.inc.php
@@ -299,6 +299,12 @@ class Page_ServerSetup extends Page
 
 		if (empty($bootentryTable)) {
 			IPxe::importLegacyMenu(true);
+			$num = IPxe::importSubnetPxeMenus('/srv/openslx/tftp/pxelinux.cfg');
+			if ($num > 0) {
+				EventLog::info('Imported old PXELinux menu, with ' . $num . ' additional IP-range based menus.');
+			} else {
+				EventLog::info('Imported old PXELinux menu.');
+			}
 			Util::redirect('?do=serversetup&show=bootentry');
 		}
 
