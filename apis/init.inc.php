4c0f8148be1008c606f5b158dd643132f50a24dd
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2019-03-25 14:33:05 +0100

    [api/init] Add logging function for crashes

--- apis/init.inc.php
+++ apis/init.inc.php
@@ -6,4 +6,10 @@
 if (!isLocalExecution())
 	exit(0);
 
+if (($report = Request::get('crashreport', false, 'string')) !== false) {
+	$details = file_get_contents($report);
+	EventLog::failure('Problems during bootup hook', $details);
+	exit(0);
+}
+
 Event::systemBooted();
